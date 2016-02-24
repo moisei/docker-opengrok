5c6baf2f0e8c3216bb9c913ee0196d370d3d5a48
moisei <moisei@gmail.com>
2016-02-24 15:10:22 +0000

    .

--- install.sh
+++ install.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
-export OPENGROK_VERSION=0.12.1
-export OPENGROK_DIR=/var/opengrok
-export OPENGROK_INSTANCE_BASE=$OPENGROK_DIR/opengrok-$OPENGROK_VERSION
+ROOT_DIR=/var/opengrok
+OG_VER=0.12.1
+APP_DIR=$ROOT_DIR/opengrok-$OG_VER
 
-mkdir -p $OPENGROK_DIR
-mkdir -p $OPENGROK_DIR/src
-mkdir -p $OPENGROK_DIR/data
-mkdir -p $OPENGROK_DIR/web/searchDalet
+mkdir -p $ROOT_DIR
+mkdir -p $ROOT_DIR/src
+mkdir -p $ROOT_DIR/data
+mkdir -p $ROOT_DIR/etc
+mkdir -p $ROOT_DIR/web/searchDalet
 
-cd $OPENGROK_DIR
+cd $ROOT_DIR
 # wget -O - https://java.net/projects/opengrok/downloads/download/$OPENGROK_BASE.tar.gz | tar zxvf -
 
-cd $OPENGROK_DIR/web/searchDalet
-tar -zxvf $OPENGROK_INSTANCE_BASE/lib/source.war
-unzip $OPENGROK_INSTANCE_BASE/lib/source.war
+cd $ROOT_DIR/web/searchDalet
+unzip $APP_DIR/lib/source.war
+cp -r $ROOT_DIR/web/searchDalet $ROOT_DIR/web/searchBrio
+cp -r $ROOT_DIR/web/searchDalet $ROOT_DIR/web/searchAmberfin
+
+sed -i 's/configuration.xml/configurationSearchDalet.xml/g'    $ROOT_DIR/web/searchDalet/WEB-INF/web.xml
+sed -i 's/configuration.xml/configurationSearchBrio.xml/g'     $ROOT_DIR/web/searchBrio/WEB-INF/web.xml
+sed -i 's/configuration.xml/configurationSearchAmberfin.xml/g' $ROOT_DIR/web/searchAmberfin/WEB-INF/web.xml
+
