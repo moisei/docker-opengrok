c2d79cdadf06e56c38e9831eefccaf2144b762ca
moisei <moisei@gmail.com>
2016-02-24 11:10:38 +0000

    adopting to multiproject configuration

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
 FROM ubuntu:14.04
 MAINTAINER Zero Cho "http://itsze.ro/"
 
-ENV DEBIAN_FRONTEND noninteractive
-ENV OPENGROK_INSTANCE_BASE /grok
+export OPENGROK_VERSION=0.12.1
+export OPENGROK_DIR=/var/opengrok
+export OPENGROK_INSTANCE_BASE=$OPENGROK_DIR/opengrok-$OPENGROK_VERSION
 
 RUN apt-get update
-RUN apt-get install -y openjdk-7-jre-headless exuberant-ctags git subversion mercurial tomcat7 wget inotify-tools
+RUN apt-get install -y openjdk-7-jre-headless exuberant-ctags git subversion mercurial tomcat7 wget unzip
 ADD install.sh /usr/local/bin/install
 RUN /usr/local/bin/install
 ADD run.sh /usr/local/bin/run
--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
-mkdir $OPENGROK_INSTANCE_BASE
-mkdir $OPENGROK_INSTANCE_BASE/data
-mkdir $OPENGROK_INSTANCE_BASE/etc
 
-wget -O - https://java.net/projects/opengrok/downloads/download/opengrok-0.12.1.tar.gz | tar zxvf -
-mv opengrok-* opengrok
-cd /opengrok/bin
-./OpenGrok deploy
+export OPENGROK_VERSION=0.12.1
+export OPENGROK_DIR=/var/opengrok
+export OPENGROK_INSTANCE_BASE=$OPENGROK_DIR/opengrok-$OPENGROK_VERSION
+
+mkdir -p $OPENGROK_DIR
+mkdir -p $OPENGROK_DIR/src
+mkdir -p $OPENGROK_DIR/data
+mkdir -p $OPENGROK_DIR/web/searchDalet
+
+cd $OPENGROK_DIR
+# wget -O - https://java.net/projects/opengrok/downloads/download/$OPENGROK_BASE.tar.gz | tar zxvf -
+
+cd $OPENGROK_DIR/web/searchDalet
+tar -zxvf $OPENGROK_INSTANCE_BASE/lib/source.war
+unzip $OPENGROK_INSTANCE_BASE/lib/source.war
